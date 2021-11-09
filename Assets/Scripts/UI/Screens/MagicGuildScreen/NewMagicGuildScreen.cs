816fcbc822eed11d6023f6883ec451e24ae0f838
OrigamiShiro <76973214+OrigamiShiro@users.noreply.github.com>
2021-11-09 17:25:00 +0300

    removed unnecessary fields

--- Assets/Scripts/UI/Screens/MagicGuildScreen/NewMagicGuildScreen.cs
+++ Assets/Scripts/UI/Screens/MagicGuildScreen/NewMagicGuildScreen.cs
@@ -15,10 +15,8 @@ namespace Overlewd
         private Button passiveSpell_2;
         private Button backButton;
 
-        private Image activeSpellUnknown;
-        private Image ultimateSpellUnknown;
-        private Image passiveSpellUnknown_1;
-        private Image passiveSpellUnknown_2;
+        private Image spellUnknown;
+       
 
         private Text mainTitle;
 
@@ -36,11 +34,6 @@ namespace Overlewd
             passiveSpell_1 = canvas.Find("PassiveSpell1").GetComponent<Button>();
             passiveSpell_2 = canvas.Find("PassiveSpell2").GetComponent<Button>();
             backButton = canvas.Find("BackButton").GetComponent<Button>();
-
-            activeSpellUnknown = activeSpell.transform.Find("UnknownSpell").GetComponent<Image>();
-            ultimateSpellUnknown = ultimateSpell.transform.Find("UnknownSpell").GetComponent<Image>();
-            passiveSpellUnknown_1 = passiveSpell_1.transform.Find("UnknownSpell").GetComponent<Image>();
-            passiveSpellUnknown_2 = passiveSpell_2.transform.Find("UnknownSpell").GetComponent<Image>();
 
             mainTitle = canvas.Find("Window").Find("MainTitle").GetComponent<Text>();
 
