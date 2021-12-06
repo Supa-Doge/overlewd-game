141f0ad0ca2c78ad383911981bd4d0badbba2d8c
OrigamiShiro <76973214+OrigamiShiro@users.noreply.github.com>
2021-12-06 17:52:43 +0300

    merged branch "OV-925_OV-929"

--- Assets/Scripts/UI/Screens/MapScreen/MapScreen.cs
+++ Assets/Scripts/UI/Screens/MapScreen/MapScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Overlewd.NSMapScreen;
@@ -12,7 +13,8 @@ namespace Overlewd
     {
         private Transform map;
         private Button chapterButton;
-
+        private Button backbutton;
+        
         private void Start()
         {
             var screenPrefab = (GameObject) Instantiate(Resources.Load("Prefabs/UI/Screens/MapScreen/MapScreen"));
@@ -22,34 +24,27 @@ namespace Overlewd
 
             var canvas = screenRectTransform.Find("Canvas");
             chapterButton = canvas.Find("ChapterButton").GetComponent<Button>();
+            backbutton = canvas.Find("BackButton").GetComponent<Button>();
 
             chapterButton.onClick.AddListener(ChapterButtonClick);
+            backbutton.onClick.AddListener(BackButtonClick);
 
             EventsWidget.CreateInstance(transform);
             QuestsWidget.CreateInstance(transform);
             BuffWidget.CreateInstance(transform);
-            SidebarButtonWidget.CreateInstance(transform);
 
             map = canvas.Find("Map");
+
+            DialogButton.GetInstance(map.Find("dialogue_1"));
+            FightButton.GetInstance(map.Find("fight_1"));
+            EventButton.GetInstance(map.Find("event_1"));
         }
 
-
-        private IEnumerable GetSpawnNodes(string nodeName)
+        private void BackButtonClick()
         {
-            int nodeId = 1;
-            var node = map.Find(nodeName + nodeId.ToString());
-
-            while (node != null)
-            {
-                yield return node;
-
-                nodeId++;
-                node = map.Find(nodeName + nodeId.ToString());
-            }
-
-            yield break;
+            UIManager.ShowScreen<CastleScreen>();
         }
-
+        
         private void ChapterButtonClick()
         {
         }
