2e5d1e85fc0658b7e972f750b965effd21df6c77
OrigamiShiro <76973214+OrigamiShiro@users.noreply.github.com>
2021-12-16 17:32:40 +0300

    added rect transform into shard's prefabs

---
+++ Assets/Scripts/UI/Screens/SummoningScreen/BaseShard.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Overlewd
+{
+    public class BaseShard : MonoBehaviour
+    {
+        protected Image back;
+        protected Image girl;
+
+        protected void Awake()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/UI/Screens/SummoningScreen/SummoningScreen.cs
+++ Assets/Scripts/UI/Screens/SummoningScreen/SummoningScreen.cs
@@ -7,18 +7,56 @@ namespace Overlewd
 {
     public class SummoningScreen : BaseScreen
     {
-        void Start()
+        private Button backButton;
+        private Button haremButton;
+        private Button portalButton;
+
+        private List<Transform> shardPositions = new List<Transform>();
+
+        private void Start()
         {
             var screenPrefab = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Screens/SummoningScreen/SummoningScreen"));
             var screenRectTransform = screenPrefab.GetComponent<RectTransform>();
             screenRectTransform.SetParent(transform, false);
             UIManager.SetStretch(screenRectTransform);
 
+            var canvas = screenRectTransform.Find("Canvas");
+
+            backButton = canvas.Find("BackButton").GetComponent<Button>();
+            haremButton = canvas.Find("HaremButton").GetComponent<Button>();
+            portalButton = canvas.Find("PortalButton").GetComponent<Button>();
+            
+            backButton.onClick.AddListener(BackButtonClick);
+            haremButton.onClick.AddListener(HaremButtonClick);
+            portalButton.onClick.AddListener(PortalButtonClick);
+            
+            Customize();
         }
 
-        void Update()
+        private void Customize()
         {
+            var shardPos = transform.Find("ShardPositions");
+            var childCount = shardPos.Find("ShardPositions").childCount;
+            
+            for (int i = 1; i <= childCount; i++)
+            {
+                shardPositions.Add(shardPos.Find($"Shard{i}"));
+            }
+        }
+        
+        private void BackButtonClick()
+        {
+            UIManager.ShowScreen<CastleScreen>();
+        }
 
+        private void HaremButtonClick()
+        {
+            UIManager.ShowScreen<HaremScreen>();
+        }
+
+        private void PortalButtonClick()
+        {
+            UIManager.ShowScreen<PortalScreen>();
         }
     }
 
