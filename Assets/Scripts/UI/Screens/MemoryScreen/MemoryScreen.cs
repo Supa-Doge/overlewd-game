e08a61705a029ed33c50840cbb8d57e7160baed1
OrigamiShiro <76973214+OrigamiShiro@users.noreply.github.com>
2021-12-16 15:37:27 +0300

    conflict fix
    This reverts commit 679584e0803d6cdf98a82ffda31f66eddf74c1f0.

--- Assets/Scripts/UI/Screens/MemoryScreen/MemoryScreen.cs
+++ Assets/Scripts/UI/Screens/MemoryScreen/MemoryScreen.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,64 +7,22 @@ namespace Overlewd
 {
     public class MemoryScreen : BaseScreen
     {
-        private Button portalButton;
-        private Button forgeButton;
-        private Button buyButton;
-        private Button backButton;
-
-        private TextMeshProUGUI commonShardsCount;
-        private TextMeshProUGUI rareShardsCount;
-        private TextMeshProUGUI legendaryShardsCount;
-        
-        private void Start()
+        void Start()
         {
             var screenPrefab = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Screens/MemoryScreen/Memory"));
             var screenRectTransform = screenPrefab.GetComponent<RectTransform>();
             screenRectTransform.SetParent(transform, false);
             UIManager.SetStretch(screenRectTransform);
 
-            var canvas = screenRectTransform.Find("Canvas");
-
-            backButton = canvas.Find("BackButton").GetComponent <Button>();
-            portalButton = canvas.Find("PortalButton").GetComponent<Button>();
-            forgeButton = canvas.Find("PortalButton").GetComponent<Button>();
-            buyButton = canvas.Find("PortalButton").GetComponent<Button>();
-
-            commonShardsCount = canvas.Find("Bag").Find("CommonShards").Find("Count").GetComponent<TextMeshProUGUI>();
-            rareShardsCount = canvas.Find("Bag").Find("RareShards").Find("Count").GetComponent<TextMeshProUGUI>();
-            legendaryShardsCount = canvas.Find("Bag").Find("LegendaryShards").Find("Count").GetComponent<TextMeshProUGUI>();
-            
-            backButton.onClick.AddListener(BackButtonClick);
-            portalButton.onClick.AddListener(PortalButtonClick);
-            forgeButton.onClick.AddListener(ForgeButtonClick);
-            buyButton.onClick.AddListener(BuyButtonClick);
-            
-            Customize();
+            screenRectTransform.Find("Canvas").Find("Girl").GetComponent<Button>().onClick.AddListener(() =>
+            {
+                UIManager.ShowScreen<GirlScreen>();
+            });
         }
 
-        private void Customize()
+        void Update()
         {
-            
-        }
 
-        private void BackButtonClick()
-        {
-            UIManager.ShowScreen<CastleScreen>();
-        }
-
-        private void PortalButtonClick()
-        {
-            UIManager.ShowScreen<PortalScreen>();
-        }
-
-        private void ForgeButtonClick()
-        {
-            
-        }
-
-        private void BuyButtonClick()
-        {
-            
         }
     }
 }
